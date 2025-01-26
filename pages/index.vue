7ab2cf8cac26d8b22fe94c8a30c7275046ef50e4
Paul Kunkel <paulik080705@gmail.com>
2025-01-26 17:10:34 +0100

    added simple chat (WIP)

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,32 @@
 <template>
   <div>
-    <h1>Bugland Testpage</h1>
+    <h1>Bugland Chat</h1>
 
-    <section>
-      <h2>/api/chat</h2>
-      <input v-model="message_id"></input>
-      <button @click="chat">Send request</button>
-      <p>{{ response }}</p>
-    </section>
+    <div class="max-w-lg flex flex-col gap-2 p-2">
+      <div v-for="message in messages" class="flex flex-col gap-2 p-2 bg-gray-200 rounded-xl">
+        {{ message.message.message_text }}
+
+        <button v-if="message.message.message_id == current" v-for="response in message.responses" @click="chat(response.response_id)" class="bg-green-300 rounded-xl p-1">
+          {{ response.response_text }}
+        </button>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts" setup>
 
-const message_id = ref("")
-const response = ref("send the request to see a result")
+const messages = ref([])
+const current = computed(() => {
+  return messages.value.at(-1).message.message_id
+})
 
-async function chat() {
-  let res = await useFetch("/api/chat", { method: "post", body: { message_id: message_id.value } })
-  response.value = res.data.value
+async function chat(id: string | null) {
+  let res = await useFetch("/api/chat", { method: "post", body: { message_id: id } })
+  messages.value.push(res.data.value)
 }
+
+chat(null)
 
 </script>
---
+++ pages/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Bugland Testpage</h1>
+
+    <section>
+      <h2>/api/chat</h2>
+      <input v-model="message_id"></input>
+      <button @click="chat">Send request</button>
+      <p>{{ response }}</p>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const message_id = ref("")
+const response = ref("send the request to see a result")
+
+async function chat() {
+  let res = await useFetch("/api/chat", { method: "post", body: { message_id: message_id.value } })
+  response.value = res.data.value
+}
+
+</script>
