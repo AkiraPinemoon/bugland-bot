df81064e534a958a199c3e9e3661464fcb056e45
Justin Kuhles <alien040@t-online.de>
2025-01-27 13:58:46 +0100

    scroll

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
     <h1>Bugland Chat</h1>
-      <div class="p-2">
-        <div v-for="message in messages" :key="message" class="flex flex-col gap-2 mb-4">
-          <Transition name="message" tag="div">
-            <div v-if="message.message.show" class="relative p-2 bg-gray-200 rounded-b-xl rounded-r-xl max-w-lg">
-              {{ message.message.message_text }}
-              <div class="absolute w-0 h-0 border-l-4 border-r-4 border-t-8 border-transparent border-t-gray-200" :style="arrowMessage"></div>
+    <div class="p-2">
+      <div v-for="message in messages" :key="message" class="flex flex-col gap-2 mb-4">
+        <Transition name="message" tag="div">
+          <div v-if="message.message.show" class="relative p-2 bg-gray-200 rounded-b-xl rounded-r-xl max-w-lg">
+            {{ message.message.message_text }}
+            <div class="absolute w-0 h-0 border-l-4 border-r-4 border-t-8 border-transparent border-t-gray-200" :style="arrowMessage"></div>
+          </div>
+        </Transition>
+        <TransitionGroup name="item" tag="button" class="self-end flex flex-col gap-2">
+          <button v-for="response in message.responses_select" :key="response.response_id" :disabled="message.message.message_id != current" @click="chat(response.response_id); message.responses_select = [response]; response.selected = true" class="self-end">
+            <div class="relative ounded-b-xl rounded-l-xl p-2 max-w-lg"
+                  :class="(message.message.message_id == current && !response.selected) ? 'bg-green-300' : 'bg-blue-300'">
+              {{ response.response_text }}
+              <div class="absolute w-0 h-0 border-l-4 border-r-4 border-t-8 border-transparent"
+                    :class="(message.message.message_id == current && !response.selected) ? 'border-t-green-300' : 'border-t-blue-300'"
+                    :style="arrowResponse"></div>
             </div>
-          </Transition>
-          <TransitionGroup name="item" tag="button" class="self-end flex flex-col gap-2">
-            <button v-for="response in message.responses_select" :key="response" :disabled="message.message.message_id != current" @click="chat(response.response_id); message.responses_select = [response]; response.selected = true" class="self-end">
-              <div class="relative ounded-b-xl rounded-l-xl p-2 max-w-lg"
-                   :class="(message.message.message_id == current && !response.selected) ? 'bg-green-300' : 'bg-blue-300'">
-                {{ response.response_text }}
-                <div class="absolute w-0 h-0 border-l-4 border-r-4 border-t-8 border-transparent"
-                     :class="(message.message.message_id == current && !response.selected) ? 'border-t-green-300' : 'border-t-blue-300'"
-                     :style="arrowResponse"></div>
-              </div>
-            </button>
-          </TransitionGroup>
-        </div>
+          </button>
+        </TransitionGroup>
+      </div>
       <div v-if="showWait">
         ...
       </div>
@@ -31,8 +31,8 @@
         <div class="absolute w-0 h-0 border-l-4 border-r-4 border-t-8 border-transparent" :style="arrowStyle"></div>
       </div>-->
     </div>
-
   </div>
+  <div ref="scrollDiv"></div>
 </template>
 
 <script lang="ts" setup>
@@ -47,6 +47,7 @@ const arrowResponse = {
   transform: "translate(0,0) rotateZ(270deg)"
 };
 
+const scrollDiv = ref<HTMLElement | null>(null)
 const showWait = ref(false)
 const messages = ref([])
 const current = computed(() => {
@@ -58,18 +59,25 @@ async function chat(id: string | null) {
 
   let res = await useFetch("/api/chat", { method: "post", body: { message_id: id } })
   messages.value.push(res.data.value)
-
-
+  scrollTo(scrollDiv)
   await new Promise(resolve => setTimeout(resolve, 1000))
+
   showWait.value = false
   res.data.value.message.show = true
   res.data.value.responses_select = []
-  await new Promise(resolve => setTimeout(resolve, 500))
+  await new Promise(resolve => setTimeout(resolve, 400))
+  scrollTo(scrollDiv)
   
   for (const response of res.data.value?.responses) {
     await new Promise(resolve => setTimeout(resolve, 100))
     res.data.value.responses_select.push(response)
   }
+  await new Promise(resolve => setTimeout(resolve, 200))
+  scrollTo(scrollDiv)
+}
+
+function scrollTo(view: Ref<HTMLElement | null>) {
+  view.value?.scrollIntoView({ behavior: "smooth" })
 }
 
 chat(null)
@@ -85,10 +93,15 @@ chat(null)
   transform: translate(-100px)
 }
 
-.item-enter-active {
+.item-move {
+  transition: transform 0.5s;
+}
+.item-enter-active,
+.item-leave-active {
   transition: all 0.5s;
 }
-.item-enter-from {
+.item-enter-from,
+.item-leave-to {
   opacity: 0;
   transform: translate(10px)
 }
