f26f40949bee2648255dedf44bb9a6e055dbab55
Justin Kuhles <alien040@t-online.de>
2025-01-27 12:44:50 +0100

    triangles

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,19 @@
       <div class="p-2">
         <div v-for="message in messages" :key="message" class="flex flex-col gap-2 mb-4">
           <Transition name="message" tag="div">
-            <div v-if="message.message.show" class="px-4 py-2 bg-gray-200 rounded-xl max-w-lg">
+            <div v-if="message.message.show" class="relative p-2 bg-gray-200 rounded-b-xl rounded-r-xl max-w-lg">
               {{ message.message.message_text }}
+              <div class="absolute w-0 h-0 border-l-4 border-r-4 border-t-8 border-transparent border-t-gray-200" :style="arrowMessage"></div>
             </div>
           </Transition>
           <TransitionGroup name="item" tag="button" class="self-end flex flex-col gap-2">
             <button v-for="response in message.responses_select" :key="response" :disabled="message.message.message_id != current" @click="chat(response.response_id); message.responses_select = [response]; response.selected = true" class="self-end">
-              <div v-if="message.message.message_id == current && !response.selected" class="bg-green-300 rounded-xl p-2 max-w-lg">
+              <div class="relative ounded-b-xl rounded-l-xl p-2 max-w-lg"
+                   :class="(message.message.message_id == current && !response.selected) ? 'bg-green-300' : 'bg-blue-300'">
                 {{ response.response_text }}
-              </div>
-              <div v-else class="bg-blue-300 rounded-xl p-2 pl-4 pr-4 max-w-lg">
-                {{ response.response_text }}
+                <div class="absolute w-0 h-0 border-l-4 border-r-4 border-t-8 border-transparent"
+                     :class="(message.message.message_id == current && !response.selected) ? 'border-t-green-300' : 'border-t-blue-300'"
+                     :style="arrowResponse"></div>
               </div>
             </button>
           </TransitionGroup>
@@ -22,15 +24,29 @@
       <div v-if="showWait">
         ...
       </div>
+      <!--
+      <div class="relative text-nowrap bg-bg border border-highlight text-white text-sm rounded py-1 px-2 shadow-lg z-50 transition-opacity duration-200 pointer-events-none"> 
+        Hallo
+        --><!-- Pfeil --><!--
+        <div class="absolute w-0 h-0 border-l-4 border-r-4 border-t-8 border-transparent" :style="arrowStyle"></div>
+      </div>-->
     </div>
 
   </div>
 </template>
 
 <script lang="ts" setup>
+const arrowMessage = {
+  top: "0",
+  right: "100%",
+  transform: "translate(0,0) rotateZ(90deg)"
+};
+const arrowResponse = {
+  top: "0",
+  left: "100%",
+  transform: "translate(0,0) rotateZ(270deg)"
+};
 
-
-const showButton = ref(true)
 const showWait = ref(false)
 const messages = ref([])
 const current = computed(() => {
@@ -38,7 +54,6 @@ const current = computed(() => {
 })
 
 async function chat(id: string | null) {
-  showButton.value = false
   showWait.value = true
 
   let res = await useFetch("/api/chat", { method: "post", body: { message_id: id } })
@@ -48,10 +63,13 @@ async function chat(id: string | null) {
   await new Promise(resolve => setTimeout(resolve, 1000))
   showWait.value = false
   res.data.value.message.show = true
+  res.data.value.responses_select = []
+  await new Promise(resolve => setTimeout(resolve, 500))
   
-  await new Promise(resolve => setTimeout(resolve, 1000))
-  res.data.value.responses_select = res.data.value.responses
-  showButton.value = true
+  for (const response of res.data.value?.responses) {
+    await new Promise(resolve => setTimeout(resolve, 100))
+    res.data.value.responses_select.push(response)
+  }
 }
 
 chat(null)
