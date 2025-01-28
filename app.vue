ec4fd7067ce312516b867c9040eb6507feeab388
Paul Kunkel <paulik080705@gmail.com>
2025-01-28 11:14:33 +0100

    added header

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import { NuxtLink } from '#components';
+
+</script>
+
 <template>
-  <NuxtPage />
+  <div class="w-full h-screen flex flex-col">
+    <header class="w-full h-20 px-6 py-2 border border-gray-200 flex gap-4 place-items-center bg-gray-100">
+      <img src="/assets/logo.png" class="h-full" />
+      <h1 class="text-5xl text-blue-200"><b>BUG</b>LAND</h1>
+      <div class="grow"></div>
+      <NuxtLink to="/">Support</NuxtLink>
+      <NuxtLink to="/edit">Admin Panel</NuxtLink>
+    </header>
+    <NuxtPage />
+  </div>
 </template>
